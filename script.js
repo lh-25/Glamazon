e70008e334c5c704e86ca0c19545ffdbef770c2c
lh-25 <sprinklecake25@gmail.com>
2024-09-27 12:55:44 -0500

    javascript almost complete, complete html, almost complete css

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 
-const search = document.querySelector("#searchButton")
-const input = document.querySelector("#textInput")
-const brandType= document.querySelector("#brand")
-const brandImg = document.querySelector("#brandImage")
-const productType = document.querySelector("#product-type")
-const productImg = document.querySelector("#productImage")
-const category = document.querySelector("#product-category")
-const tag = document.querySelector("#product-tag")
-productData = ''
-brandData = ''
+const search = document.querySelector('#searchButton')
+const input = document.querySelector('#textInput')
+const resultsGrid = document.querySelector('.results-grid')
 
 const getProductName = async (productName) => {
     let product = await axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${productName}`)
      productData = product.data 
-     console.log(product)
+     resultsGrid.innerHTML = ''
+
     if (productData.length > 0) {
     for (let i = 0; i < productData.length; i++) {
-        let item = document.createElement('p')
-        item.innerHTML =`Product Name: ${productData[i].name}`
-        productType.appendChild(item)
+        const productDiv = document.createElement('div')
+        productDiv.classList.add('result-item')
+        const productName = productData[i].name
+        const brandName = productData[i].brand
+        const productTags = productData[i].tag_list.length > 0 ?
+        productData[i].tag_list.join(",") : 'No tags available'
+        const productImage = productData[i].api_featured_image
+        const productLink = productData[i].product_link
+
+        productDiv.innerHTML = `<img src="${productImage}" alt="${productName}">
+        <h3> ${productName}</h3> <p>${brandName}</p> <p>${productTags}</p> <a href="${productLink}" target="_blank>Buy Here</a>`
+        resultsGrid.appendChild(productDiv)
     }
     } 
 }
 
-const getBrandName = async (brandName) => {
-    let brand = await axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandName}`)
-    brandData = brand.data
-    console.log(brand)
-}
-
 search.addEventListener('click', () => {
-     if (getProductInfo()) {
-        getProductName(input.value)
-     } else {
-    getBrandName(input.value)}
+     getProductName(input.value)
+
 })
